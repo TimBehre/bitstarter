4a2f5f35da596d806e2bbcdc6320f99776ab3462
Tim Behre <tim.behre@gmail.com>
2013-07-10 19:16:35 +0000

    alles

--- web.js
+++ web.js
@@ -1,25 +1,25 @@
 
 
-var  port = process.env.PORT || 5000;
-app.listen(port, function() {
-console.log("Listening on " + port);
-});
+//var  port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//console.log("Listening on " + port);
+//});
 
-var express = require('express');
-
-
-var app = express.createServer(express.logger());
+//var express = require('express');
 
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+//var app = express.createServer(express.logger());
 
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//app.get('/', function(request, response) {
+//  response.send('Hello World!');
+//});
+
+
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
 
 
       
