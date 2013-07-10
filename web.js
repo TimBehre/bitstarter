a50ed0ea5dd0d248a8fd41c6e49210394ef0b662
Tim Behre <tim.behre@gmail.com>
2013-07-10 19:03:49 +0000

    alles

--- web.js
+++ web.js
@@ -1,44 +1,47 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-var fs = require("fs");
-app.get('/', function(request, response) {
-//  response.send("Hallo Welt")});
-
-if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-fs.readFile(filename, "binary", function(err, file) {
-
-      if(err) {        
-
-        response.writeHead(500, {"Content-Type": "text/plain"});
-
-        response.write(err + "\n");
-
-        response.end();
-
-
-        return;
-
-
-      }
-
-
-      response.writeHead(200);
-
-
-      response.write(file, "binary");
-
-
-      response.end();
-
-
+var libpath = require('path'),
+    http = require("http"),
+    fs = require('fs'),
+    url = require("url"),
+    mime = require('mime');
+ 
+var path = ".";
+var port = 8088;
+ 
+http.createServer(function (request, response) {
+ 
+    var uri = url.parse(request.url).pathname;
+    var filename = libpath.join(path, uri);
+ 
+    libpath.exists(filename, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
+ 
+        if (fs.statSync(filename).isDirectory()) {
+            filename += '/index.html';
+        }
+ 
+        fs.readFile(filename, "binary", function (err, file) {
+            if (err) {
+                response.writeHead(500, {
+                    "Content-Type": "text/plain"
+                });
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
+ 
+            var type = mime.lookup(filename);
+            response.writeHead(200, {
+                "Content-Type": type
+            });
+            response.write(file, "binary");
+            response.end();
+        });
     });
-});
-
-
-var  port = process.env.PORT || 5000;
-app.listen(port, function() {
-console.log("Listening on " + port);
-});
-      
+}).listen(port);
