71415aba83d3ee68e224e7c6a050478ce596d8ae
Tim Behre <tim.behre@gmail.com>
2013-07-10 19:25:51 +0000

    alles

--- web.js
+++ web.js
@@ -1,70 +1,37 @@
+var http = require("http"),
+url = require("url"),
+path = require("path"),
+fs = require("fs")
+port = process.argv[2] || 5000;
 
+http.createServer(function(request, response) {
 
-//var  port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//console.log("Listening on " + port);
-//});
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
 
-//var express = require('express');
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
 
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
-//var app = express.createServer(express.logger());
-
-
-//app.get('/', function(request, response) {
-//  response.send('Hello World!');
-//});
-
-
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//});
-
-
-      
-var express = require('express');
-
-var app = express.createServer(express.logger());
-var fs = require("fs");
-var filename = files[i];
-
-app.get('/', function(request, response) {
-
-
-if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-fs.readFile(filename, "binary", function(err, file) {
-
+    fs.readFile(filename, "binary", function(err, file) {
       if(err) {        
-
         response.writeHead(500, {"Content-Type": "text/plain"});
-
         response.write(err + "\n");
-
         response.end();
-
-
         return;
-
-
       }
 
-
       response.writeHead(200);
-
-
       response.write(file, "binary");
-
-
       response.end();
-
-
     });
-});
+  });
+}).listen(parseInt(port, 10));
 
-
-var  port = process.env.PORT || 5000;
-app.listen(port, function() {
-console.log("Listening on " + port);
-});
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
