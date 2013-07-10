185c64bea7eab86cd37c332124f062bc76b44948
Tim Behre <tim.behre@gmail.com>
2013-07-10 12:58:04 +0000

    Added web.js

--- web.js
+++ web.js
@@ -1,14 +1,25 @@
-var express = require('express');
+//var express = require('express');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send("Hallo Welt")});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+//app.get('/', function(request, response) {
+//  response.send("Hallo Welt")});
+
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
+
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(index);
+}).listen(9615);
 
 
 
